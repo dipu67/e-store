d873e29b0910d2d869e5be1c2d5a7db68faaf2b5
dipu67 <mddipu2482@gmail.com>
2025-06-27 09:14:10 +0600

    fixed

--- src/app/api/products/createproduct/route.ts
+++ src/app/api/products/createproduct/route.ts
@@ -6,10 +6,10 @@ export async function POST(req: Request) {
   await connectDB();
   const body = await req.json();
   const id = await Product.find().countDocuments() + 1;
-  const { title,discription,price,discountPrice,image,stock } = body;
+  const { title,description,price,discountPrice,image,stock } = body;
   try {
     const slug = title.toLowerCase().replace(/\s+/g, "-");
-    const newPost = await Product.create({id, title, slug,discription,price,discountPrice,image,stock });
+    const newPost = await Product.create({id, title, slug,description,price,discountPrice,image,stock });
     return NextResponse.json(newPost, { status: 201 });
   } catch (err) {
     console.error("Error creating blog:", err);
